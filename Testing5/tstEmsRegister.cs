23381c538bc21f8e159f4727402a767ae2cb11a2
Rohit <P2779735@mydmu.ac.uk>
2024-05-16 16:37:16 +0100

    Lab 6 test for register created

---
+++ Testing5/tstEmsRegister.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstEmsRegister
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //New instance of Register
+            clsRegister clsNewRegister = new clsRegister();
+
+            //test to see that it exists
+            Assert.IsNotNull(clsNewRegister);
+        }
+    }
+}
