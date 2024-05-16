89802a839d6cb9ae0e89f76185203a7e2ed1fb41
Mije31 <162947912+Mije31@users.noreply.github.com>
2024-05-16 14:35:16 +0100

    Added tests to Order class

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,7 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
         public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int OrderID { get; set; }
+        public int CountyCode { get; set; }
+        public string HouseNo { get; set; }
     }
 }
