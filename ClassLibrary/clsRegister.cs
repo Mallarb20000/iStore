cff07662be02c19508f4d61345159e02b157ffa5
Rohit <P2779735@mydmu.ac.uk>
2024-05-20 14:28:31 +0100

    Setting up test

--- ClassLibrary/clsRegister.cs
+++ ClassLibrary/clsRegister.cs
@@ -1,7 +1,27 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsRegister
     {
+        private Int32 mE_Id;
         public string username { get; set; }
+        public int E_Id {
+            get 
+            {
+                //to return data of the property
+                return mE_Id;
+            } set 
+            {
+                mE_Id = value;  
+        
+            }
+        }
+
+        public bool Find(int E_Id)
+        {
+            mE_Id = 1;
+            return true;
+        }
     }
 }
--- Testing5/tstEmsRegister.cs
+++ Testing5/tstEmsRegister.cs
@@ -16,5 +16,54 @@ namespace Testing5
             //test to see that it exists
             Assert.IsNotNull(clsNewRegister);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //New instance of Register
+            clsRegister clsNewRegister = new clsRegister();
+            //Creating bollian to store results of the validation
+            Boolean Found = false;
+
+            //create some test data to use this method
+            Int32 E_Id = 1;
+
+            //invoke the method
+            Found = clsNewRegister.Find(E_Id);
+
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestE_IdFound()
+        {
+            //create new instance of register
+            clsRegister clsNewRegister = new clsRegister();
+            //Creating bollian to store results of the validation
+            Boolean Found = false;
+
+            //create a boolean variable to recoerd if the data is OK
+
+            Boolean OK = true;
+
+            //create some test data to use this method
+            Int32 E_Id = 1;
+
+            //invoke the method
+            Found = clsNewRegister.Find(E_Id);
+
+            //check E_id
+            if (clsNewRegister.E_Id !=1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+
+
+
+
+        }
     }
 }
