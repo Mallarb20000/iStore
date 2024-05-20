27a2eab62968bf38b3a1ffe96f54c0367bc21dc0
eylulozun <eylulozun.ozdem@gmail.com>
2024-05-20 15:09:55 +0100

    Added the interface

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -2,5 +2,13 @@
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductSalary { get; set; }
+        public string ProductImg { get; set; }
     }
 }
