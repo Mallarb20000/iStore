305216ab70e7218e75e6a6d7761caa6eb6d6d020
Rohit Malla <P2779735@my365.dmu.ac.uk>
2024-05-29 18:11:20 +0100

    Hasibe class issue fixed

---
+++ ClassLibrary/clsCms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCms
+    {
+        public int CustomerID { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+
+        public string CustomerPassword { get;  set; }
+        public bool Active { get; set; }
+        public string PostCode { get; set; }
+
+        public bool Find(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
