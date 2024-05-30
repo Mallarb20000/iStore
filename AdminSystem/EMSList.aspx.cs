8b8643d310fe00bf1d75b8c74df89f69e5300047
Rohit Malla <P2779735@my365.dmu.ac.uk>
2024-05-30 19:47:28 +0100

    Added list view and connected to the database

---
+++ AdminSystem/EMSList.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+public partial class _1_List : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if this page is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayRegister();
+        }
+
+
+    }
+
+    void DisplayRegister()
+    {
+        //create a instance of the register collection
+        clsRegisterCollection AllRegister = new clsRegisterCollection();    
+
+        //set the data source to list of employee in the collection
+        lstRegisterList.DataSource = AllRegister.RegisterList;
+        // set the name of the primary key
+        lstRegisterList.DataValueField = "E_Id";
+        //set the data field to display
+        lstRegisterList.DataTextField="Name";
+        //bind data to the list 
+        lstRegisterList.DataBind();
+    }
+}
