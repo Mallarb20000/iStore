1eb1a986e8f31d2c4d21419077ae2305a3ad46b8
eylulozun <eylulozun.ozdem@gmail.com>
2024-06-03 17:43:29 +0100

    12 is done

--- Testing1/tstStockCollection.cs
+++ Testing1/tstStockCollection.cs
@@ -7,8 +7,10 @@ using System.Collections.Generic;
 namespace Testing1
 {
     [TestClass]
-    public class tstStockCollection 
+    public class tstStockCollection
     {
+        
+
         [TestMethod]
         public void IstanceOK()
         {
@@ -21,8 +23,8 @@ namespace Testing1
         }
 
         [TestMethod]
-        public void    StockListOK() 
-        
+        public void StockListOK()
+
         {
             //create an instance of the class we want to create 
             clsStockCollection AllStock = new clsStockCollection();
@@ -45,10 +47,70 @@ namespace Testing1
             //assign the data to the property
             AllStock.StockList = TestList;
             //test to see that the vlaues 
-           Assert.AreEqual(AllStock.StockList, TestList);
+            Assert.AreEqual(AllStock.StockList, TestList);
         }
 
-       
+
+
+
+
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            clsStock TestStock = new clsStock();
+            //set its properties
+            TestStock.Active = true;
+            TestStock.ProductID = 1;
+            TestStock.ProductName = "mac1";
+            TestStock.ProductPrice = 1000;
+            TestStock.ProductDescription = "12 inch";
+            TestStock.ProductQuantity = 1;
+            TestStock.ProductImg = null;
+            //assign the data to the property
+            AllStock.ThisStock = TestStock;
+            //test to see that the vlaues 
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create 
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be list of object
+            List<clsStock> TestList = new List<clsStock>();
+            //Add an Item to the List 
+            //create the item of test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.Active = true;
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "mac1";
+            TestItem.ProductPrice = 1000;
+            TestItem.ProductDescription = "12 inch";
+            TestItem.ProductQuantity = 1;
+            TestItem.ProductImg = null;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStock.StockList = TestList;
+            //test to see that the vlaues 
+            Assert.AreEqual(AllStock.Count, TestList.Count);
+
+
+        } 
+
+      
+      
+
+
+
+
 
     }
 }
